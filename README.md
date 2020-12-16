3605748e6b707919fdc08ae8b98d1db2d1db1502
Philipp Heckel <pheckel@datto.com>
2020-12-16 15:13:16 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# Datto RMM :: FireEye Red Team Countermeasure Scanner
+Build 20, 16th December 2020, (C) Copyright Datto, Inc.
+
+## BACKSTORY
+
+On the 8th of December, a group of security auditors and consultants within FireEye's internal "Red Team" were hacked by a sophisticated state actor. In the attack, tools used by Red Team to test vulnerabilities in systems being audited were exfiltrated.
+Following the news of the hack, FireEye decided to release countermeasures to the public to help detect whether stolen Red Team code has been used by malicious actors to leverage attacks on their devices. 
+
+Using FireEye's countermeasures, Datto are making available a script which scans a system for executables showing signatures associated with Red Team's code. This is intended to give administrators peace of mind, both that the stolen code is not being used in the wild and that their systems are not being actively attacked with it.
+
+Presence of an infected file does not necessarily imply a successful attack; if a device is properly patched, Red Team's tools are ineffectual. FireEye have provided a list of CVEs to patch against in order to guarantee protection against their exploits.
+The list is: https://github.com/fireeye/red_team_tool_countermeasures/blob/master/CVEs_red_team_tools.md
+
+## TECHNICAL INFORMATION
+
+This script was originally developed for use with the Datto RMM product. It has been released for other RMM vendors to distribute to their partners via their own script distribution methods.
+
+It uses the YARA scanning tool by VirusTotal alongside a YARA definition set from FireEye to scan executable files on Windows systems for signs of code known to FireEye to belong to their Red Team toolset.
+It is written in and compatible with PowerShell versions 2.0 onward (Windows 7 ships with PowerShell 2.0).
+
+The files "yara32.exe", "yara64.exe" and "all-yara.yar" must be attached to the script. They are invoked from the same directory as the location of the script file.
+The file "buildXX.ps1", where XX is a number, is the script in question.
+
+The script expects an environmental variable to be mapped at $env:usrScanScope. This variable should have a value of 1 to 4, (ideally) with the partner able to select which option. "1" only scans executables running in memory at execution time. "2" scans executables present on the Home Drive (usually C:\); "3" scans executables on all fixed and removable drives. "4" scans executables on ALL accessible drives.
+
+Option 1 should conclude within ten minutes. Expect options 2-4 to take upwards of an hour depending on the amount of data present.
+
+If you edit this script, please preserve the credits for Datto RMM and the author (seagull).
+
+## CREDITS
+
+This script was written by seagull for Datto RMM. The Component was written for the Datto RMM ComStore.
+YARA is a project by VirusTotal. The "yara-COPYING.txt" file is included at their request. GitHub: https://github.com/VirusTotal/yara/
+The YARA definitions were released by FireEye. GitHub: https://github.com/fireeye/red_team_tool_countermeasures
+
+https://www.datto.com/rmm
+
---
+++ build20.ps1
@@ -0,0 +1,102 @@
+﻿<#fireEye red team vuln scanner :: build 20/seagull (Datto RMM) :: user variables: usrScanScope
+provided for other RMMs for the good of the community :: preserve all seagull and Datto RMM credits#>
+
+#scanning function
+
+function doScan {
+    param ([parameter(mandatory=$false,ValueFromPipeline=$true)]$list)
+    process {
+        #catch both type-1 and alternative use-cases
+        if ($env:usrScanScope -eq 1) {$file=$list.Path} else {$file=$list.FullName}
+
+        #add it to the logfile, with a pause for handling
+        try {
+            Add-Content "log.txt" -Value $file -ErrorAction Stop
+        } catch {
+            Start-Sleep -Seconds 1
+            Add-Content "log.txt" -Value $file -ErrorAction SilentlyContinue
+        }
+
+        #scan it
+        clear-variable yaResult -ErrorAction SilentlyContinue
+        $yaResult=cmd /c "yara$varch.exe `"all-yara.yar`" `"$file`" -s"
+        if ($yaResult) {
+            write-host "====================================================="
+            $script:varDetection=1
+            write-host "! DETECTION:"
+            write-host $yaResult
+        }
+    }
+}
+
+write-host "FireEye Red Team Countermeasures (by Datto)"
+write-host "====================================================="
+
+[string]$varch=[intPtr]::Size*8
+$script:varDetection=0
+
+#check to make sure yara will run on the host device
+cmd /c "yara32.exe -v" >nul 2>&1
+$varExit=$LASTEXITCODE
+if ($varExit -ne 0) {
+    write-host "! ERROR: Yara was unable to run on this device."
+    write-host "  The Visual C++ Redistributable is required in order to use YARA."
+    if ($env:CS_CC_HOST) {
+        write-host "  An installer Component is available from the ComStore."
+    }
+    exit 1
+}
+
+#make a logfile
+
+% {$host.ui.WriteErrorLine("Output may be truncated; results are stored in $PWD\log.txt.")}
+% {$host.ui.WriteErrorLine("=====================================================")}
+set-content -Path "log.txt" -Force -Value "Files scanned:"
+Add-Content "log.txt" -Value "====================================================="
+
+#check the files are there
+
+if (!(test-path yara32.exe -ErrorAction SilentlyContinue) -or !(test-path yara64.exe -ErrorAction SilentlyContinue) -or !(test-path all-yara.yar -ErrorAction SilentlyContinue)) {
+    write-host "! ERROR: YARA binary files were not found attached to Component."
+    write-host "  Execution cannot continue."
+    exit 1
+}
+
+#input variable "usrScanScope": 1: memory / 2: home drive / 3: all fixed/remov drives / 4: all accessible drives
+
+if ($env:usrScanScope -eq 2) {
+    write-host "- Scan scope: All EXEs present on home drive (Intensive)."
+    get-childitem -Path $env:HOMEDRIVE\* -Force -Recurse -include *.exe -ErrorAction SilentlyContinue | select FullName | ? {$_.FullName} | doScan
+} elseif ($env:usrScanScope -eq 3) {
+    write-host "- Scan scope: All EXEs present on ALL fixed or removable drives (Very intensive)."
+    Get-WmiObject -Class Win32_logicaldisk | ? {$_.DriveType -eq 2 -or $_.DriveType -eq 3} | ? {$_.FreeSpace} | % {
+        get-childitem -Path "$($_.DeviceID)\*" -Force -Recurse -include *.exe -ErrorAction SilentlyContinue | select FullName | ? {$_.FullName} | doScan
+    }
+} elseif ($env:usrScanScope -eq 4) {
+    write-host "- Scan scope: All EXEs present on ALL drives of all types (Most intensive)."
+    Get-WmiObject -Class Win32_logicaldisk | ? {$_.FreeSpace} | % {
+        get-childitem -Path "$($_.DeviceID)\*" -Force -Recurse -include *.exe -ErrorAction SilentlyContinue | select FullName | ? {$_.FullName} | doScan
+    }
+} else {
+    write-host "- Scan scope: All EXEs running in memory (Default)."
+    get-process | select -unique | select Path | ? {$_.Path} | doScan
+}
+
+if ($script:varDetection -eq 1) {
+    write-host "====================================================="
+    write-host "! Files containing FireEye exploit code have been found on the system."
+} else {
+    write-host "- No files matching FireEye exploit code were found on the system."
+}
+
+#close out
+
+write-host "- Scan completed."
+write-host "  A list of all scanned files has been written to the StdErr stream."
+
+write-host "- Ensure devices are up-to-date with the CVE mitigation list:"
+write-host '  https://github.com/fireeye/red_team_tool_countermeasures/blob/master/CVEs_red_team_tools.md'
+
+get-content log.txt | % {$host.ui.WriteErrorLine($_)}
+
+exit $script:varDetection
